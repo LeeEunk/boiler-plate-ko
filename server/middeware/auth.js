c2eb88c697bb3716a8517b8831003ac50510847e
eunkyunglee <dldmsrud7817@gmail.com>
2020-09-11 14:14:10 +0900

    완성

--- server/middeware/auth.js
+++ server/middeware/auth.js
@@ -4,8 +4,6 @@ const { User } = require('../models/User');
 let auth = (req, res, next) => {
 
     //인증 처리를 하는 곳
-
-
     // 클라이언트 쿠키에서 토큰을 가져온다.
     let token = req.cookies.x_auth;
 
@@ -19,7 +17,6 @@ let auth = (req, res, next) => {
         next();
     })
     // 유자가 있으면 인증 Okay
-
     // 유저가 없으면 인증 No!
 }
 
